ab2e34272c4dafe743d12768839498482b9adef5
nitzanpap <nitzanpap@gmail.com>
2022-06-30 13:51:19 +0300

    Fixed indentaion

--- src/app.ts
+++ src/app.ts
@@ -28,13 +28,13 @@ function search(e: any) {
 }
 
 async function getPokemonList() {
-    for (let i = 0; i < 3; i++) {
-        const res = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${12}&offset=${i * 12}`);
-        const pokemonsJSON = await res.json();
-        const pokemonArr = pokemonsJSON.results.map((obj: object) => {
-            return obj;
-        });
-        console.log(pokemonArr);
-    }
+  for (let i = 0; i < 3; i++) {
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${12}&offset=${i * 12}`);
+    const pokemonsJSON = await res.json();
+    const pokemonArr = pokemonsJSON.results.map((obj: object) => {
+      return obj;
+    });
+    console.log(pokemonArr);
+  }
 }
 getPokemonList();
