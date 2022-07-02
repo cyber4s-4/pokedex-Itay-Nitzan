bb62a167d0764a5b798540621084c7e033494ea9
Itay Meitav <itai9999@gmail.com>
2022-07-03 00:14:34 +0300

    working code

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,83 @@
-import { Pokemon } from './Pokemon';
 import { createAndDisplayPokemons } from "./handleUI";
 
-function api(pokemon: string) {
+let pokemon;
+if (pokemon == undefined) {
+    pokemon = "pikachu";
+    api(pokemon);
+} else {
+    api(pokemon);
+}
+export function api(pokemon: string) {
     fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
         .then((res) => res.json())
         .then((data) => {
             const pic = document.querySelector('#pokeimg') as HTMLImageElement;
-            pic.src = data.sprites.front_default; // TODO
-            (document.getElementById('name') as HTMLElement).innerText = data.name; // TODO
+            pic.src = data.sprites.front_default;
+            function spinShiny() {
+                const spinButton = document.getElementsByClassName('spin')[0];
+                const shinyButton = document.getElementsByClassName('shiny')[0];
+                let pokeImgRegularFront = data.sprites.front_default;
+                let pokeImgRegularBack = data.sprites.back_default;
+                let pokeImgShinyFront = data.sprites.front_shiny;
+                let pokeImgShinyBack = data.sprites.back_shiny;
+                spinButton.addEventListener('click', () => {
+                    console.log('spin', pic.src);
+
+                    if (pic.src = pokeImgRegularFront) {
+                        pic.src = pokeImgRegularBack
+                        return;
+                    }
+                    if (pic.src = pokeImgRegularBack) {
+                        console.log('Back to regular front');
+
+                        pic.src = pokeImgRegularFront
+                        return;
+                    }
+                    if (pic.src = pokeImgShinyFront) {
+                        pic.src = pokeImgShinyBack
+                        return;
+                    }
+                    if (pic.src = pokeImgShinyBack) {
+                        pic.src = pokeImgShinyFront
+                        return;
+                    }
+                })
+                shinyButton.addEventListener('click', () => {
+                    console.log('shine/unshine');
+
+                    if (pic.src = pokeImgRegularFront) {
+                        pic.src = pokeImgShinyFront
+                        return;
+                    }
+                    if (pic.src = pokeImgRegularBack) {
+                        pic.src = pokeImgShinyBack
+                        return;
+                    }
+                    if (pic.src = pokeImgShinyFront) {
+                        pic.src = pokeImgRegularFront
+                        return;
+                    }
+                    if (pic.src = pokeImgShinyBack) {
+                        pic.src = pokeImgRegularBack
+                        return;
+                    }
+                });
+            }
+            spinShiny();
+            (document.getElementById('name') as HTMLElement).innerText = data.name;
             const type = document.querySelector('#type') as HTMLElement;
-            type.textContent = `${data.types[0].type.name} type pokemon`; // TODO
+            type.textContent = `${data.types[0].type.name} type pokemon`;
+            let moves = document.getElementsByClassName("moves-container")[0] as HTMLElement;
+            for (let i = 0; i < 6; i++) {
+                let li = moves.getElementsByClassName("move")[i];
+                li.innerHTML = data.moves[i * 8].move.name;
+            }
             const height = document.querySelector('#height') as HTMLElement;
-            height.innerText = `Height: ${data.height}`; // TODO
+            height.innerText = `Height: ${data.height}`;
             const weight = document.querySelector('#weight') as HTMLElement;
-            weight.innerText = `Weight: ${data.weight}`; // TODO
-            const submit = document.querySelector('.submit') as HTMLElement;
-            submit.addEventListener('click', search);
+            weight.innerText = `Weight: ${data.weight}`;
+            let form = document.querySelector("form");
+            form!.addEventListener("submit", search);
         });
 }
 
@@ -25,17 +87,5 @@ function search(e: any) {
     api(pokeName);
 }
 
-async function getPokemonList() {
-    for (let i = 0; i < 3; i++) {
-        const res = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${12}&offset=${i * 12}`);
-        const pokemonsJSON = await res.json();
-        const pokemonArr = pokemonsJSON.results.map((obj: object) => {
-            return obj;
-        });
-        console.log(pokemonArr);
-    }
-}
-getPokemonList();
-const pikachu = new Pokemon('pikachu');
-console.log(pikachu);
+
 createAndDisplayPokemons();
--- src/handleUI.ts
+++ src/handleUI.ts
@@ -1,6 +1,7 @@
+import { api } from "./app";
 export function createAndDisplayPokemons() {
 
-    const poke_container = document.getElementById('poke-container-body') as HTMLElement;
+    const poke_container = document.getElementsByClassName('poke-container-body')[0];
     const pokemon_count = 100;
     const colors = {
         fire: '#FDDFDF',
@@ -19,51 +20,73 @@ export function createAndDisplayPokemons() {
         normal: '#F5F5F5'
     }
 
-    const main_types = Object.keys(colors)
+    const main_types = Object.keys(colors);
 
     const fetchPokemons = async () => {
         for (let i = 1; i <= pokemon_count; i++) {
-            await getPokemon(i)
+            await getPokemon(i);
         }
     }
 
-
     const getPokemon = async (id: number) => {
-        const url = `https://pokeapi.co/api/v2/pokemon/${id}`
-        const res = await fetch(url)
-        const data = await res.json()
-        createPokemonCard(data)
+        const url = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+        const data = await url.json();
+        createPokemonCard(data);
     }
 
     const createPokemonCard = (pokemon: any) => {
-        const pokemonEl = document.createElement('div')
-        pokemonEl.classList.add('pokemon')
-
-        const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1)
-        const id = pokemon.id.toString().padStart(3, '0')
-
-        const poke_types = pokemon.types.map(type => type.type.name)
-        const type = main_types.find(type => poke_types.indexOf(type) > -1)
-        const color = colors[type]
+        const pokemonEl = document.createElement('div') as HTMLDivElement;
+        pokemonEl.classList.add('pokemon');
+        const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
+        const id = pokemon.id.toString().padStart(3, '0');
+        const poke_types = pokemon.types.map((type: any) => type.type.name);
+        const type = main_types.find(type => poke_types.indexOf(type) > -1);
+        const color = colors[type];
 
         pokemonEl.style.backgroundColor = color;
 
-        const pokemonInnerHTML = `
-  <div class="img-container">
+        pokemonEl.innerHTML = `
+        <div class="img-parent">
+        <div class="img-container">
         <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${id}.png" alt="">
         </div>
-    <div class="info">
-    <span class="number">#${id}</span>
+        </div>
+        <div class="info">
+        <span class="number">#${id}</span>
         <h3 class="name">${name}</h3>
         <small class="type">Type: <span>${type}</span> </small>
-    </div>
-    `
+        </div>`
 
-        pokemonEl.innerHTML = pokemonInnerHTML
+        poke_container.appendChild(pokemonEl);
+    }
 
-        poke_container.appendChild(pokemonEl)
+    async function cardClickEvent() {
+        await fetchPokemons();
+        const pokemonCards = Array.from(document.getElementsByClassName("pokemon"));
+
+        for (let i = 0; i < pokemonCards.length; i++) {
+            pokemonCards[i].addEventListener('click', () => {
+                const name = pokemonCards[i].getElementsByClassName("name")[0].innerHTML.toLowerCase();
+                api(name);
+            });
+        }
+    }
+
+    function getRandom(min: number, max: number) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        const randomNumber = Math.floor(Math.random() * (max - min + 1) + min);
+        return randomNumber;
     }
 
 
-    fetchPokemons()
+    function randomSearch() {
+        const randomButton = document.getElementsByClassName('random')[0];
+        randomButton.addEventListener('click', () => {
+            api(getRandom(1, pokemon_count).toString())
+        });
+    }
+    cardClickEvent();
+    randomSearch();
 }
+
