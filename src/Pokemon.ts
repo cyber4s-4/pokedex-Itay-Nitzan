c0a8cf353b9fa1d6230ad1f02291c58398e5d7a0
nitzanpap <nitzanpap@gmail.com>
2022-07-02 12:07:54 +0300

    Reverted broken changes to fetch function

--- src/Pokemon.ts
+++ src/Pokemon.ts
@@ -2,18 +2,33 @@ export class Pokemon {
   pictureSrc: string | null;
   id: number | null;
   name: string | null;
-  type: string | null;
+  pokemonType: string | null;
   height: string | null;
   weight: string | null;
   moves: string | null;
+  rawData: any;
 
   constructor(name: string) {
+    this.rawData = this.getDataFromLocalStorage();
     this.pictureSrc = null;
     this.id = null;
     this.name = name;
-    this.type = null;
+    this.pokemonType = null;
     this.height = null;
     this.weight = null;
     this.moves = null;
+  }
+
+  async fetchPokemonData() {
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${this.name}`);
+    const pokemonDataJSON = await res.json();
+    this.id = pokemonDataJSON.id;
+    this.height = pokemonDataJSON.height;
+    this.weight = pokemonDataJSON.weight;
+    this.pictureSrc = `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${this.id}.png`;
+    this.pokemonType = pokemonDataJSON.types[0].type.name;
+  }
+  getDataFromLocalStorage(): object {
+    return JSON.parse(localStorage.getItem('pokemonsData') as string);
   }
 }
--- src/app.ts
+++ src/app.ts
@@ -12,15 +12,16 @@ async function retrieveAllPokemonsToDB() {
     const pokemonJSON = await res.json();
     for (let i = 0; i < pokemonJSON.results.length; i++) {
       const newPokemon = new Pokemon(pokemonJSON.results[i].name);
+      await newPokemon.fetchPokemonData();
       pokemonArr.push(newPokemon);
     }
     console.log('Finished retrieving all the data from the API.');
-    localStorage.setItem('pokemonsData', JSON.stringify(pokemonArr));
     // Otherwise, get the pokemon array from the local storage.
   } else {
     console.log('Retrieved data from local storage.');
     pokemonArr = JSON.parse(localStorage.getItem('pokemonsData') as string);
   }
+  localStorage.setItem('pokemonsData', JSON.stringify(pokemonArr));
 }
 retrieveAllPokemonsToDB();
 createAndDisplayPokemons();
