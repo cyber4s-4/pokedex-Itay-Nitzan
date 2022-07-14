cca45a1fc52d1783901cee6dc515f7e38a60d5c6
nitzanpap <nitzanpap@gmail.com>
2022-07-15 00:08:51 +0300

    Organized the script js

--- script.js
+++ script.js
@@ -1,78 +1,44 @@
-const { MongoClient, Collection } = require("mongodb");
+const { MongoClient, Collection, ServerApiVersion } = require('mongodb');
+const fs = require('fs');
 
-const fs = require("fs");
-
-let data = fs.readFileSync("./pokemonData.json", "utf-8");
-
+let data = fs.readFileSync('./pokemonData.json', 'utf-8');
 const uri =
-  "mongodb+srv://itay234:abc12345@cluster0.one6i.mongodb.net/?retryWrites=true&w=majority";
-// const local = "mongodb://localhost:27017/?authMechanism=DEFAULT";
+  'mongodb+srv://nitzanpap:ilovecode@cluster0.nercoqf.mongodb.net/?retryWrites=true&w=majority';
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
-const client = new MongoClient(uri);
-run()
-// Collection.prototype.findById = function (id) {
-//   return this.findOne({ id });
-// };
+run();
 
-function run() {
+async function run() {
   let arr = [];
   let counter = 906;
 
   addOriginalToArray(arr);
   addAllFusionsToArray(arr, counter);
-
-  // console.log(counter)
-  // arr.splice(0, arr.length - 3025);
-
-  // await pokemons.insertMany(arr.splice(0, 3025), {
-  //   maxTimeMS: 99999,
-  // });
-  // if (false)
   await addArrayToDb(arr);
-}
-
-async function addArrayToDb(arr) {
-  try {
-    await client.connect();
-    let pokedex = client.db("pokedex");
-    let pokemons = pokedex.collection("pokemons");
-    pokemons.insertMany(arr, {
-      maxTimeMS: 99999,
-    });
-} catch(e) {
-  console.error(e);
-  } finally {
-     await client.close();
-   }
 }
 
 function addOriginalToArray(arr) {
   data = JSON.parse(data);
-  arr.push(data);
+  arr.push(...data);
 }
+
 function addAllFusionsToArray(arr, counter) {
   for (let i = 0; i < 905; i++) {
     for (let j = 0; j < 905; j++) {
-      if (i !== j) arr.push(combinePokemons(arr[i], arr[j], counter++));
+      if (i !== j) {
+        arr.push(combinePokemons(arr[i], arr[j], counter++));
+      }
     }
   }
 }
 
-// async function insertOriginal() {
-//   let pokedex = client.db("pokedex");
-//   let pokemons = pokedex.collection("pokemons");
-//   data = JSON.parse(data);
-//   await pokemons.insertMany(data, {
-//     maxTimeMS: 99999,
-//   });
-// }
-// insertOriginal().then(() => client.close());
-
 function combinePokemons(pok1, pok2, id) {
-  // let random = Math.floor(Math.random() * 2);
-  // console.log(pok1);
-  let pokemon = {
-    name: pok1.name + "/" + pok2.name,
+  return {
+    name: pok1.name + '/' + pok2.name,
     rawData: null,
     id: id,
     height: Math.floor((pok1.height + pok2.height) / 2).toString(),
@@ -90,5 +56,25 @@ function combinePokemons(pok1, pok2, id) {
     pokemonTypes: [pok1.pokemonTypes[0], pok2.pokemonTypes[0]],
     spritesSources: {},
   };
-  return pokemon;
+}
+
+async function addArrayToDb(arr) {
+  try {
+    await client.connect();
+    // Declare db and its collection
+    let pokedex = client.db('pokedex');
+    let pokemons = pokedex.collection('pokemons');
+    // Clear any previous data in the collection and redeclare the collection
+    pokemons.drop();
+    pokemons = pokedex.collection('pokemons');
+
+    // Insert all pokemons, originals and fusions, into the db.
+    await pokemons.insertMany(arr, {
+      maxTimeMS: 99999,
+    });
+  } catch (e) {
+    console.log(e);
+  } finally {
+    await client.close();
+  }
 }
--- src/server/server.ts
+++ src/server/server.ts
@@ -2,85 +2,90 @@ const express = require('express');
 const path = require('path');
 const cors = require('cors');
 const fs = require('fs');
-import { getAllPokemons, getPokemonSearch, getAllStars, RemoveStars, AddStars, SearchStars } from './mongo';
+import {
+  getAllPokemons,
+  getPokemonSearch,
+  getAllStars,
+  RemoveStars,
+  AddStars,
+  SearchStars,
+} from './mongo';
 import { Request, Response } from 'express';
 const app = express();
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../../dist')));
 app.use(express.urlencoded({ extended: true }));
 app.use(
-    cors({
-        credentials: true,
-        origin: true,
-    })
+  cors({
+    credentials: true,
+    origin: true,
+  })
 );
-
 
 let favorites: object[] = [];
 
-
 app.get('/pokemons', async (req: Request, res: Response) => {
-    try {
-        return res.status(201).json(await getAllPokemons().catch(console.error));
-    } catch {
-        return res.status(400).send({ message: 'Error' });
-    }
+  try {
+    return res.status(201).json(await getAllPokemons().catch(console.error));
+  } catch {
+    return res.status(400).send({ message: 'Error' });
+  }
 });
 
 app.get('/:searchValue', async (req: Request, res: Response) => {
-    // If request contains a number
-    if (!isNaN(Number(req.params.searchValue))) {
-        try {
-            const pokemonID = Number(req.params.searchValue);
-            const dataPokemon = await getPokemonSearch(pokemonID);
-            if (!dataPokemon) {
-                return res.status(400).send({ message: 'Pokemon not found' });
-            } else {
-                return res.status(201).json(dataPokemon);
-            }
-        } catch {
-            return res.status(400).send({ message: 'Error' });
-        }
-        // If request contains a string
-    } else {
-        try {
-            const pokemonName = req.params.searchValue.toLowerCase();
-            const dataPokemon = await getPokemonSearch(pokemonName);
-            if (!dataPokemon) {
-                return res.status(400).send({ message: 'Pokemon not found' });
-            } else {
-                return res.status(201).json(dataPokemon);
-            }
-        } catch {
-            return res.status(400).send({ message: 'Error' });
-        }
+  // If request contains a number
+  if (!isNaN(Number(req.params.searchValue))) {
+    try {
+      const pokemonID = Number(req.params.searchValue);
+      const dataPokemon = await getPokemonSearch(pokemonID);
+      if (!dataPokemon) {
+        return res.status(400).send({ message: 'Pokemon not found' });
+      } else {
+        return res.status(201).json(dataPokemon);
+      }
+    } catch {
+      return res.status(400).send({ message: 'Error' });
     }
+    // If request contains a string
+  } else {
+    try {
+      const pokemonName = req.params.searchValue.toLowerCase();
+      const dataPokemon = await getPokemonSearch(pokemonName);
+      if (!dataPokemon) {
+        return res.status(400).send({ message: 'Pokemon not found' });
+      } else {
+        return res.status(201).json(dataPokemon);
+      }
+    } catch {
+      return res.status(400).send({ message: 'Error' });
+    }
+  }
 });
 
 app.post('/star', async (req: Request, res: Response) => {
-    try {
-        const pokemonSearch = await SearchStars(req.body.name.toLowerCase());
-        if (pokemonSearch == true) {
-            await RemoveStars(req.body.name.toLowerCase());
-            return res.status(202).send({ message: 'Removed from favorites' });
-        } else {
-            await AddStars(req.body.name.toLowerCase());
-            res.status(201).send({ message: 'Added to favorites' });
-        }
-    } catch {
-        res.status(500).send({ message: 'Error' });
+  try {
+    const pokemonSearch = await SearchStars(req.body.name.toLowerCase());
+    if (pokemonSearch == true) {
+      await RemoveStars(req.body.name.toLowerCase());
+      return res.status(202).send({ message: 'Removed from favorites' });
+    } else {
+      await AddStars(req.body.name.toLowerCase());
+      res.status(201).send({ message: 'Added to favorites' });
     }
+  } catch {
+    res.status(500).send({ message: 'Error' });
+  }
 });
 
 app.get('/star/star', async (req: Request, res: Response) => {
-    try {
-        return res.status(201).json(await getAllStars().catch(console.error));
-    } catch {
-        return res.status(400).send({ message: 'Error' });
-    }
+  try {
+    return res.status(201).json(await getAllStars().catch(console.error));
+  } catch {
+    return res.status(400).send({ message: 'Error' });
+  }
 });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`listening on port ${port}`);
+  console.log(`listening on port ${port}`);
 });
