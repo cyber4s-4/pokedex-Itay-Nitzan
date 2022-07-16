74d70e231b17fd3cb9ea6d04e98871d390acc5c6
nitzanpap <nitzanpap@gmail.com>
2022-07-16 18:48:48 +0300

    Refactored gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,143 +1,123 @@
-const gulp = require("gulp");
-const clean = require("gulp-clean");
-const rename = require("gulp-rename");
-const webpack = require("webpack-stream");
-const sass = require("gulp-sass")(require("sass"));
+const gulp = require('gulp');
+const clean = require('gulp-clean');
+const rename = require('gulp-rename');
+const webpack = require('webpack-stream');
+const sass = require('gulp-sass')(require('sass'));
 // const browserSync = require("browser-sync").create();
-const { exec, execSync, execFile } = require("child_process");
-const webpackConfig = require("./webpack.config.js");
-const { task } = require("gulp");
+const { exec, execSync, execFile } = require('child_process');
+const webpackConfig = require('./webpack.config.js');
+const { task } = require('gulp');
 
 // Removes previous dist
-gulp.task("clean", () => {
+gulp.task('clean', () => {
   return gulp
-    .src("./dist", {
+    .src('./dist', {
       allowEmpty: true,
     })
     .pipe(clean());
 });
 
 // copies all files exept ts and scss
-gulp.task("copy-no-transpile", () => {
-  return gulp
-    .src(["src/**/*", "!src/**/*.ts", "!src/**/*.scss"])
-    .pipe(gulp.dest("./dist/"));
+gulp.task('copy-no-transpile', () => {
+  return gulp.src(['src/**/*', '!src/**/*.ts', '!src/**/*.scss']).pipe(gulp.dest('./dist/'));
 });
 
 // Converts scss to css
-gulp.task("transpile-scss", () => {
-  return gulp
-    .src("./src/client/*.scss")
-    .pipe(sass())
-    .pipe(gulp.dest("./dist/client/"));
+gulp.task('transpile-scss', () => {
+  return gulp.src('./src/client/*.scss').pipe(sass()).pipe(gulp.dest('./dist/client/'));
 });
 
 // Converts ts to js
-gulp.task("transpile-ts", (cb) => {
-  exec("tsc", (err, msg) => {
+gulp.task('transpile-ts', (cb) => {
+  exec('tsc', (err, msg) => {
     cb();
   });
 });
 
 // Creates js bundle from client js files
-gulp.task("bundle-client-js", () => {
-  return webpack(webpackConfig).pipe(gulp.dest("./dist/client"));
+gulp.task('bundle-client-js', () => {
+  return webpack(webpackConfig).pipe(gulp.dest('./dist/client'));
 });
 
 // Transfers static files
-gulp.task("copy-server-js", () => {
-  return gulp.src(["dist/tsc/server/**/*.js"]).pipe(gulp.dest("./dist/server"));
+gulp.task('copy-server-js', () => {
+  return gulp.src(['dist/tsc/server/**/*.js']).pipe(gulp.dest('./dist/server'));
 });
 
 // Creates/Updates the dist folder
 gulp.task(
-  "build",
+  'build',
   gulp.series(
-    "clean",
-    "copy-no-transpile",
-    "transpile-scss",
-    "transpile-ts",
-    "bundle-client-js",
-    "copy-server-js"
+    'clean',
+    'copy-no-transpile',
+    'transpile-scss',
+    'transpile-ts',
+    'bundle-client-js',
+    'copy-server-js'
   )
 );
 
 // Watch static files
-gulp.task("watch-static", () => {
+gulp.task('watch-static', () => {
   return gulp.watch(
-    ["src/**/*", "!src/**/*.ts", "!src/**/*.scss"],
-    gulp.series("copy-no-transpile")
+    ['src/**/*', '!src/**/*.ts', '!src/**/*.scss'],
+    gulp.series('copy-no-transpile')
   );
 });
 
 // Watch scss files
-gulp.task("watch-scss", () => {
-  return gulp.watch("./src/client/**/*.scss", gulp.series("transpile-scss"));
+gulp.task('watch-scss', () => {
+  return gulp.watch('./src/client/**/*.scss', gulp.series('transpile-scss'));
 });
 
 // Watch ts files and recompile
-gulp.task("watch-ts", () => {
-  exec("tsc -w");
+gulp.task('watch-ts', () => {
+  exec('tsc -w');
 });
 
 // Watch tsc client files
-gulp.task("watch-client-js", () => {
-  return gulp.watch(
-    "./dist/tsc/client/**/*.js",
-    gulp.series("bundle-client-js")
-  );
+gulp.task('watch-client-js', () => {
+  return gulp.watch('./dist/tsc/client/**/*.js', gulp.series('bundle-client-js'));
 });
 
 // Watch tsc setver files
-gulp.task("watch-server-js", () => {
-  return gulp.watch("./dist/tsc/server/**/*.js", gulp.series("copy-server-js"));
+gulp.task('watch-server-js', () => {
+  return gulp.watch('./dist/tsc/server/**/*.js', gulp.series('copy-server-js'));
 });
 
 // Watch all files
 gulp.task(
-  "watch",
-  gulp.parallel(
-    "watch-static",
-    "watch-scss",
-    "watch-ts",
-    "watch-client-js",
-    "watch-server-js"
-  )
+  'watch',
+  gulp.parallel('watch-static', 'watch-scss', 'watch-ts', 'watch-client-js', 'watch-server-js')
 );
 
 // start nodemon
-gulp.task("nodemon", () => {
-  exec("nodemon dist/server/server.js");
-  exec("google-chrome http://localhost:3000");
+gulp.task('nodemon', () => {
+  exec('nodemon dist/server/server.js');
+  exec('google-chrome http://localhost:3000');
 });
 
 // Run all together
-gulp.task("default", gulp.series("build", gulp.parallel("watch", "nodemon")));
+gulp.task('default', gulp.series('build', gulp.parallel('watch', 'nodemon')));
 
-gulp.task("clean-deploy", () => {
+gulp.task('clean-deploy', () => {
   return gulp
-    .src(["./deploy/"], {
+    .src(['./deploy/'], {
       allowEmpty: true,
     })
     .pipe(clean());
 });
 
-gulp.task("copy-dist-to-deploy", () => {
+gulp.task('copy-dist-to-deploy', () => {
   return gulp
-    .src([
-      "./dist/**/*",
-      "!./dist/cache/**/*",
-      "!./dist/tsc/**/*",
-      "!./dist/cache",
-      "!./dist/tsc",
-    ])
-    .pipe(gulp.dest("./deploy/dist"));
+    .src(['./dist/**/*', '!./dist/cache/**/*', '!./dist/tsc/**/*', '!./dist/cache', '!./dist/tsc'])
+    .pipe(gulp.dest('./deploy/dist'));
 });
 
-gulp.task("copy-node-to-deploy", () => {
+gulp.task('copy-node-to-deploy', () => {
   return gulp
-    .src(["./package.json", "./package-lock.json", "./Procfile"])
-    .pipe(gulp.dest("./deploy"));
+    .src(['./package.json', './package-lock.json', './Procfile'])
+    .pipe(gulp.dest('./deploy'));
 });
 
 // task('deploy-heroku', (cb) => {
@@ -149,12 +129,12 @@ gulp.task("copy-node-to-deploy", () => {
 // })
 
 gulp.task(
-  "deploy",
+  'deploy',
   gulp.series(
-    "build",
-    "clean-deploy",
-    "copy-dist-to-deploy",
-    "copy-node-to-deploy"
+    'build',
+    'clean-deploy',
+    'copy-dist-to-deploy',
+    'copy-node-to-deploy'
     // 'deploy-heroku'
   )
 );
